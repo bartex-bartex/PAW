64542ec30939324894ee5da5b0bf5b067fed9368
Bartosz Warchoł <bartex132@gmail.com>
2024-12-10 23:24:26 +0100

    Zadanie2 final behavior for chechboxes

--- lab9/Zadanie2/product-list/src/app/app.component.ts
+++ lab9/Zadanie2/product-list/src/app/app.component.ts
@@ -6,16 +6,26 @@ import { CommonModule } from '@angular/common'
   selector: 'app-root',
   imports: [CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css'] // Fixed typo (from 'styleUrl' to 'styleUrls')
 })
 export class AppComponent implements OnInit {
   data: Products = {};
 
   expandedCategories: Set<string> = new Set(); // Tracks expanded categories
   selectedCategories: Set<string> = new Set(); // Tracks selected categories
-  selectedProducts: Set<string> = new Set(); // Tracks selected products
+  selectedProducts: { [category: string]: Set<string> } = {}; // Tracks selected products by category
   
   constructor(private productService: ProductService) { }
+
+  ngOnInit(): void {
+    this.productService.getProducts().subscribe((json) => {
+      this.data = json;
+      // Initialize selected products per category
+      Object.keys(this.data).forEach(category => {
+        this.selectedProducts[category] = new Set();
+      });
+    });
+  }
 
   categories(): string[] {
     return Object.keys(this.data);
@@ -34,18 +44,32 @@ export class AppComponent implements OnInit {
   }
 
   toggleCategorySelection(category: string): void {
+    if (this.expandedCategories.has(category)) {
+      return; // Don't allow toggling if category is expanded
+    }
+
     if (this.selectedCategories.has(category)) {
       this.selectedCategories.delete(category);
+      this.selectedProducts[category].clear();
     } else {
       this.selectedCategories.add(category);
+      // Select all products within that category
+      this.selectedProducts[category] = new Set(this.products(category));
     }
   }
 
-  toggleProductSelection(product: string): void {
-    if (this.selectedProducts.has(product)) {
-      this.selectedProducts.delete(product);
+  toggleProductSelection(category: string, product: string): void {
+    if (this.selectedProducts[category].has(product)) {
+      this.selectedProducts[category].delete(product);
     } else {
-      this.selectedProducts.add(product);
+      this.selectedProducts[category].add(product);
+    }
+
+    // If all products are selected, select the category
+    if (this.selectedProducts[category].size === this.products(category).length) {
+      this.selectedCategories.add(category);
+    } else {
+      this.selectedCategories.delete(category);
     }
   }
 
@@ -57,17 +81,7 @@ export class AppComponent implements OnInit {
     return this.selectedCategories.has(category);
   }
 
-  isProductSelected(product: string): boolean {
-    return this.selectedProducts.has(product);
-  }
-
-  ngOnInit(): void {
-    this.productService.getProducts().subscribe((json) => {
-      // console.log(JSON.stringify(data));
-      // console.log(data["sweets"]);
-      this.data = json;
-      console.log(Object.keys(this.data));
-      // console.log(this.products["Sweets"].length);
-    });
+  isProductSelected(category: string, product: string): boolean {
+    return this.selectedProducts[category]?.has(product);
   }
 }
